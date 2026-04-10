8f3b112b0a6bd3b74319a07828612d5d80a4aa00
Grd-gael <gaelgrandval@gmail.com>
2026-04-10 10:07:37 +0200

    js

---
+++ app.js
@@ -0,0 +1,10 @@
+export function dropdown() {
+
+    let container = document.querySelector("#derouleAtelier")
+
+    container.innerHTML = `
+    <h1> Déroulé de l'atelier</h1>
+    <div>Dropdown</div>
+    `
+
+}
